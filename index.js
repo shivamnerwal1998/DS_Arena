bcd9febe8c618a9f5ecbf133d3fd717669167712
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-06-03 15:45:47 +0530

    Adding components

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
-import App from "./App" ;
+import React from "react";
+import ReactDOM from "react-dom";
+import Content from "./Components/Contents";
+import Data from "./Data/ContentsData";
+import "./index.css";
 
+ReactDOM.render(<>
 
-ReactDOM.render(
-  <>
-    <BrowserRouter>
-    <App/>
-    </BrowserRouter>
-  </>
-  ,
-  document.getElementById('root')
-);
+  <Content Link={Data[0].Link} Title={Data[0].Title} About={Data[0].Description} />
+  <Content Link={Data[1].Link} Title={Data[1].Title} About={Data[1].Description} />
+  <Content Link={Data[2].Link} Title={Data[2].Title} About={Data[2].Description} />
+  <Content Link={Data[3].Link} Title={Data[3].Title} About={Data[3].Description} />
 
+</>,
+  document.getElementById("root")
 
+)
