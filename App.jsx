f23d0ca740674408210c612423f4089ec4f1002b
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-06-07 08:00:53 +0530

    Adding stack components  to Browser Router

--- App.jsx
+++ App.jsx
@@ -6,6 +6,9 @@ import React from "react" ;
 import Stack from "./Components/Stack/Stack" ;
 import AllCont from "./AllCont";
 import NavBar from "./NavBar/NavBar" ; 
+import Push from "./Components/Stack/Push"; 
+import Pop from "./Components/Stack/Pop";
+import Peek from "./Components/Stack/Peek";  
 import {Switch,Route} from "react-router-dom" ;
 
 
@@ -16,6 +19,11 @@ const App = () => {
         <Switch>
         <Route exact path="/" component={AllCont}/>
         <Route exact path="/Stack" component={Stack}/>
+        <Route exact path="/stack/push" component={Push}/>
+        <Route exact path="/stack/pop" component={Pop}/>
+        <Route exact path="/stack/peek" component={Peek}/>
+
+
         </Switch>
     </div>
     return elem ;
