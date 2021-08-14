db9ffde551b76e3fa8c949966e573f685b3efe1b
Shivam Nerwal <60189715+shivamnerwal1998@users.noreply.github.com>
2021-08-14 22:57:10 +0530

    Delete Components directory

--- Components/LinkedList.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react" ;
-import StackNavBar from "../NavBar/StackNavBar"; 
-const LinkedList =()=>{
-    const elem =<> 
-     <h1>Hello </h1>
-    </>
-    return elem ; 
-}
-export default LinkedList; 
